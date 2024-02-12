5a344c9b228ee2399751c479076932c36add9d72
addict4ever <127635398+addict4ever@users.noreply.github.com>
2024-02-12 06:02:43 -0500

    Update reconwin.h

--- reconwin/reconwin.h
+++ reconwin/reconwin.h
@@ -27,5 +27,3 @@ void writeFooter(std::ofstream& html_file);
 void writeSection(std::ofstream& html_file, const std::string& section_id, const std::string& title, const std::string& content);
 void writeSectionLink(std::ofstream& html_file, const std::string& section_id, const std::string& link_text);
 void writeTableFromCommandOutput(std::ofstream& html_file, const std::string& section_id, const std::string& title, const std::string& command_output);
-void classifyProcesses(std::unordered_map<std::string, std::string>& system_outputs);
-
