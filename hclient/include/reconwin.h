6a642c727baa3aa99303d5b0187bd692aaee7f24
addict4ever <127635398+addict4ever@users.noreply.github.com>
2024-02-12 06:38:57 -0500

    Create reconwin.h

---
+++ hclient/include/reconwin.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <cstdio>
+#include <unordered_map>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <filesystem>
+#include <Windows.h>
+#include <Dsgetdc.h> 
+#include <lm.h>
+#include <algorithm>
+ 
+
+namespace fs = std::filesystem;
+
+bool isDomainJoined();
+bool isAdmin();
+std::vector<std::string> searchFilesByExtension(const std::string& directory, const std::string& extension);
+void executePowerShellCommand(const std::string& command, std::unordered_map<std::string, std::string>& output_map, const std::string& key);
+void executeCommand(const char* command, std::string& output);
+void writeHeader(std::ofstream& html_file);
+void writeFooter(std::ofstream& html_file);
+void writeSection(std::ofstream& html_file, const std::string& section_id, const std::string& title, const std::string& content);
+void writeSectionLink(std::ofstream& html_file, const std::string& section_id, const std::string& link_text);
+void writeTableFromCommandOutput(std::ofstream& html_file, const std::string& section_id, const std::string& title, const std::string& command_output);
